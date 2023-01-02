52a55d0096efce2794723e82b13abad6b9d190a2
Mark O'Grady <meogrady@yahoo.com>
2023-01-01 17:22:41 -0800

    Created locator stubs for each of the quick launch items.

--- test/ohrm_dashboard_tests.py
+++ test/ohrm_dashboard_tests.py
@@ -13,7 +13,7 @@ def test_verify_page(firefox_driver):
     assert ohrm_dashboard.ohrm_dashboard_page_header_title.get_text() == 'Dashboard'
     firefox_driver.quit()
 
-'''
+
 def test_quick_launch_assign_leave(firefox_driver):
     # Create objects, go to login page
     ohrm_login = OhrmLoginPage(firefox_driver)
@@ -25,36 +25,34 @@ def test_quick_launch_assign_leave(firefox_driver):
     ohrm_login.ohrm_submit_button.click()
 
     # Link 'Assign Leave' Icon from Quick Launch group.
-    quick_launch_menu = OhrmQuickLaunchMenu(driver=firefox_driver)
-    quick_launch_menu.ohrm_navigate_to_assign_leave_page().click()
+    quick_launch_menu = OhrmDashboardPage(firefox_driver)
+    quick_launch_menu.navigate_to_assign_leave_from_quick_launch().click()
 
-    # Get text from page and assert
+    '''# Get text from page and assert
     assign_leave = OhrmAssignLeave(driver=firefox_driver)
     text = assign_leave.assign_leave_page_title().get_text()
 
     assert text, "Assign Leave"
+    '''
 
     firefox_driver.quit()
-'''
 
-'''
+
 def test_quick_launch_leave_list(firefox_driver):
-    pass
+    firefox_driver.quit()
 
 
 def test_quick_launch_timesheets(firefox_driver):
-    pass
+    firefox_driver.quit()
 
 
 def test_quick_launch_apply_leave(firefox_driver):
-    pass
+    firefox_driver.quit()
 
 
 def test_quick_launch_my_leave(firefox_driver):
-    pass
+    firefox_driver.quit()
 
 
 def test_quick_launch_my_timesheet(firefox_driver):
-    pass
-
-'''
+    firefox_driver.quit()
